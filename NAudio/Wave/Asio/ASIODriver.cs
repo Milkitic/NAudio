642c231c1007621eebb47320f3cf1622a0f603af
Milkitic <Milkitic@126.com>
2020-11-23 16:22:39 +0800

    remove to compile

--- NAudio/Wave/Asio/ASIODriver.cs
+++ NAudio/Wave/Asio/ASIODriver.cs
@@ -23,7 +23,7 @@ namespace NAudio.Wave.Asio
         {
         }
 
-#if NETSTANDARD2_0 || NET35 || NETCOREAPP3_0
+#if NETSTANDARD2_0 || NET35 || NETCOREAPP3_0 || NET5_0
         /// <summary>
         /// Gets the ASIO driver names installed.
         /// </summary>
